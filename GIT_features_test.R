4ece00e286990691fd609a0882065bc66426e8c0
mcvdwh <mcvdwh@proton.me>
2025-03-07 14:32:29 +1100

    Adding UNIX commands

--- GIT_features_test.R
+++ GIT_features_test.R
@@ -3,3 +3,19 @@
 # AUTHOR: mcvdwh (7♠2♦)
 #
 # See GIT metadata on latest edit date
+
+# UNIX Commands
+
+# ls #list dir content
+# mkdir folder_name #create directory called "folder_name"
+# rmdir folder_name  #remove an empty directory as long as it is empty
+# rm -r folder_name  #remove dir that is not empty, "r" stands for recursive
+
+# cd: change dir
+# ../ # two dots represents parent dir
+# . # single dot represents current workingdir 
+# 
+# cd ~/projects # concatenate with forward slashes
+# cd ../.. # change to two parent layer beyond
+# cd -  # whatever dir you were before
+# cd  # return to the home dir
