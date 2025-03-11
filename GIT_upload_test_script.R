b7614cdfe3c0b984ad2a3d8712988100a55a986b
mcvdwh <mcvdwh@proton.me>
2025-03-11 15:08:45 +1100

    test

--- GIT_upload_test_script.R
+++ GIT_upload_test_script.R
@@ -3,8 +3,17 @@
 install.packages("tidyverse")
 install.packages("dslabs")
 
+# add extra nonsense in here
+
 library(tidyverse)
 library(dslabs)
 
 data("murders")
 head(murders)
+
+class(murders)
+murderstibble <- as.tibble(murders)
+
+murderstibble %>% # add a note in line
+   mutate(region = as.character(region)) %>%
+   summarise(pop = sum(population))
